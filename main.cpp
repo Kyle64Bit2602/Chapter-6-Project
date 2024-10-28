1e3222db51a2dffde47dcde316e51741af8c9094
Kyle64Bit2602 <kylebackup64@gmail.com>
2024-10-28 14:46:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 #include <cmath>
 using namespace std;
 
+int totalDays();
+int departTime();
+int airfaireCost();
+int carRentals();
+int conferenceFees();
+int hotelExpenses();
+int meals();
+
 int main()
 {
 
