a14ad8fdf0c18158518d63cdfecb7d4f84196607
MGRichardson <142840558+MGRichardson@users.noreply.github.com>
2024-10-29 14:49:28 -0500

    Update departTime.cpp

--- departTime/departTime.cpp
+++ departTime/departTime.cpp
@@ -13,10 +13,11 @@ double meals();
 
 int main()
 {
-
+	double leave, arrival;
+	departTime(leave, arrival);
 }
 
-void departTime()
+void departTime(double& leave, double& arrival)
 {
 	/* 
 	Accepts 2 arguments
@@ -24,5 +25,10 @@ void departTime()
 	and the time of arrival back home on the last day of the trip.
 	*/
 
+	cout << "What time did you depart for the trip (ex: 705 is 7:05):  ";
+	cin >> leave;
+
+	cout << "What time did you return from the trip (ex: 724 is 7:24): ";
+	cin >> arrival;
 
 }
