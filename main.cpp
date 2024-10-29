d37a997424d6c2869a08e41a9c3d60c2024013c9
Kyle64Bit2602 <kylebackup64@gmail.com>
2024-10-29 14:45:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,32 @@ double meals();
 
 int main()
 {
+	/*
+	---MAIN---
+	accepts no arguments
+	calculates the total cost of the trip
+	using all of the functions
+	and outputs them
+	returns 0
+	*/
+
+	double totalCosts;
+
+	cout << "Welcome to the Travel Expenses Calculator.\n";
+	cout << "Please fill out every necessary field.\n";
+
+
+	int days = totalDays();
+	string leave, arrival = departTime();
+	double airCost = airfaireCost();
+	double conference = conferenceFees();
+	double rentals = carRentals();
+	double hotel = hotelExpenses();
+	double meal = meals();
+
+	totalCosts = airCost + rentals
+		+ rentals + hotel + meal;
+
 
 }
 
