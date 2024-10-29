88d4003aad802bd513e39364a1a3bb0e68a82deb
MGRichardson <142840558+MGRichardson@users.noreply.github.com>
2024-10-29 14:29:11 -0500

    pineapple

---
+++ departTime/departTime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int totalDays();
+void departTime();
+double airfaireCost();
+double carRentals();
+double conferenceFees();
+double hotelExpenses();
+double meals();
+
+int main()
+{
+
+}
+
+void departTime()
+{
+	/* 
+	Accepts 2 arguments
+	The time of departure on the first day of the trip,
+	and the time of arrival back home on the last day of the trip.
+	*/
+
+
+}
