c6530d108e666a426b7d2ea23555b5b6a282ec7f
GideonPeine <142840190+GideonPeine@users.noreply.github.com>
2024-10-30 14:44:34 -0500

    STUFF

--- meals/meals/meals.cpp
+++ meals/meals/meals.cpp
@@ -7,6 +7,32 @@
 using namespace std;
 
 
+
+
+double departTimeM()
+{
+    double leave, arrival;
+    /*
+    Accepts 0 arguments
+    The time of departure on the first day of the trip,
+    and the time of arrival back home on the last day of the trip.
+    returns leave and arrival time, with 1 digit seperation on average 
+    */
+
+    cout << "What time did you depart for the trip (ex: 705 is 7:05):  ";
+    cin >> leave;
+
+    cout << "What time did you return from the trip (ex: 724 is 7:24): ";
+    cin >> arrival;
+
+    return leave*10000+ arrival;
+}
+
+
+
+
+
+
 double hotel()
 {
     double expense;
@@ -32,26 +58,102 @@ double hotel()
 double meal()
 {
 
-    int leave, arrival;
+    int dep=0, arrival;
     double breakfast=0, lunch=0, dinner=0;
-    cin >> leave;
-    cin >> arrival;
+    arrival = departTimeM();
+    dep = arrival / 10000;
+    arrival = (arrival - (dep * 10000));
+    cout <<  arrival << " " << dep;
 
-    if (leave < 700)
-    {
+    do {
         cout << "Enter Cost of breakfast: ";
         cin >> breakfast;
-    }
-    if (leave < 1200)
-    {
+
+    } while (breakfast < 0);
+    do{
         cout << "Enter Cost of lunch: ";
         cin >> lunch;
+
+    } while (lunch < 0);
+    do {
+        cout << "Enter Cost of dinner: ";
+        cin >> dinner;
+
+    } while (dinner < 0);
+    if (dep < 700)
+    {
+        if (breakfast - 9 < 0)
+        {
+            breakfast = 9;
+        }
+        breakfast -= 9;
+    }
+    if (arrival >100)
+    {
+        if (lunch - 12 < 0)
+        {
+            lunch = 12;
+        }
+        lunch -= 12;
     }
     if (arrival >700)
     {
-        cout << "Enter Cost of dinner: ";
-        cin >> dinner;
+        if (dinner -16 < 0)
+        {
+            dinner = 16;
+        }
+        dinner -= 16;
     }
+
+
+    return breakfast+ lunch+ dinner;
+
+
+
+
+}
+
+
+
+double carRentals()
+{
+    int days = totalDays();
+    double distance, mile, parking, taxi, paral, taxal;
+    paral = days * 6, taxal = days * 10;
+    do {
+        cout << "\nHow far have you driven, in miles : ";
+        cin >> distance;
+
+    } while (distance < 0);
+    do {
+        cout << "\nEnter Parking fees: \n";
+        cin >> parking;
+
+    } while (distance < 0);
+    do {
+        cout << "\nEnter taxi costs: \n";
+        cin >> taxi;
+
+    } while (distance < 0);
+    
+    if (parking - paral < 0)
+    {
+        parking = paral;
+    }
+    parking -= paral;
+    if (taxi - taxal < 0)
+    {
+        taxi = taxal;
+    }
+    taxi-= taxal;
+
+    mile = distance * 0.27;
+
+    return mile + parking + taxi;
+
+
+
+
 
 
 
@@ -63,7 +165,7 @@ double meal()
 
 int main()
 {
-    cout << hotel();
+    cout << carRentals();
 }
 
 
